8d61785d6c125abd9688f656c4dd593168be3879
Filipe Tielson <filipe_thielsom@hotmail.com>
2023-07-24 12:36:45 -0300

    fix: Add subpath to StoryBookon production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -20,7 +20,7 @@ const config = {
   },
   viteFinal:(config, {configType})=> {
     if (configType === 'PRODUCTION'){
-      config.base = '/Design-System'
+      config.base = '/Design-System/'
     }
 
     return config
